bee93fd111bd5b3aac10fe393fbf3299c7ef47a7
Matej Horvat <matej.horvat@gmail.com>
2018-12-24 21:18:10 +0100

    Solution to day 3

--- 3/solution.py
+++ 3/solution.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
 import collections
 
+
+def parse_claim(claim):
+    id, cut =  claim.split(' @ ')
+    cut = cut.split(': ')
+    off = [int(s) for s in cut[0].split(',')]
+    siz = [int(s) for s in cut[1].split('x')]
+    return [id] + off + siz
+
+
 # read instructions
-claims = [line.rstrip('\n') for line in open('input.txt')]
+cuts = list(map(parse_claim, [line.rstrip('\n') for line in open('input.txt')]))
 
 
 def main():
-
     d = collections.defaultdict(int)
+
     # part 1
-    for c in claims:
-        cut = c.split(' @ ')[1].split(': ')
-        off = [int(s) for s in cut[0].split(',')]
-        siz = [int(s) for s in cut[1].split('x')]
-        for i in range(0, siz[0]):
-            for j in range(0, siz[1]):
-                d[off[0] + i + 1000*(off[1] + j)] += 1
-    x = [f for f in d.values() if f > 1]
-    print(len(x))
+    for c in cuts:
+        for i in range(0, c[3]):
+            for j in range(0, c[4]):
+                d[c[1] + i + 1000*(c[2] + j)] += 1
+    print(len([f for f in d.values() if f > 1]))
+
+    # part 2
+    for c in cuts:
+        single = 1
+        for i in range(0, c[3]):
+            for j in range(0, c[4]):
+                if d[c[1] + i + 1000*(c[2] + j)] > 1:
+                    single = 0
+        if single:
+            print(c)
+            return
 
 
 if __name__ == '__main__':
