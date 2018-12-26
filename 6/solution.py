249e69f6171e9de84ab5e62ec0816c1063695052
Matej Horvat <matej.horvat@gmail.com>
2018-12-26 11:28:11 +0100

    Partial solution to 6

---
+++ 6/solution.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+# read input
+input = [line.rstrip('\n') for line in open('input.txt')]
+
+
+def main():
+    print(input)
+    return
+
+
+if __name__ == '__main__':
+    main()
